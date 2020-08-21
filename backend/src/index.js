e545c4a05479e6c82faaf4a329071615ec7e169c
Joao Forbrig <joaoforbrig.96@gmail.com>
2020-08-21 15:10:37 -0300

    oi

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const { response } = require('express')
 const {uuid, isUuid} = require('uuidv4')
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
 
 const projects = []
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default function Header(){
-   return <header>
-        <h1>ReactJS</h1>
-          </header>
+export default function Header({title}){
+   return (
+      <header>
+        <h1>{title}</h1>
+
+         
+
+      </header>
+   )
 }
